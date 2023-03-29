760eead3a13ec1b45a5aff2a0a02dd5905a8b9e4
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2023-03-29 14:39:58 -0600

    setup and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        'soft-blue': 'hsl(231, 69%, 60%)',
+        'soft-red': 'hsl(0, 94%, 66%)',
+        'grayish-blue': 'hsl(229, 8%, 60%)',
+        'very-dark-blue': 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+  },
 };
